eff94c1c78b50320ed6e5c963c93c3b88188e56d
Koundinya Goparaju <kg@speedledger.se>
2019-07-11 21:58:48 +0200

    Fixed incorrect column type

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS campaigns
          id       BIGINT PRIMARY KEY AUTO_INCREMENT,
          name     VARCHAR(255) NOT NULL,
          budget   DECIMAL      NOT NULL,
-         spending BOOLEAN      NOT NULL
+         spending DECIMAL      NOT NULL
      );
 
 CREATE TABLE IF NOT EXISTS keywords
